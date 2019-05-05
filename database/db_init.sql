db57d6e22c66608e6029f32eb9922cfeb1e8e5dc
shanenolanwit <shane.nolan.wit@gmail.com>
2019-05-05 21:27:45 +0100

    updated database structure in int code

--- database/db_init.sql
+++ database/db_init.sql
@@ -1,11 +1,20 @@
-
-CREATE TABLE IF NOT EXISTS `beer` (
+DROP TABLE IF EXISTS `beer`;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `beer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255),
-  `review` VARCHAR(255),
-  `latitude` DECIMAL(10, 8) NOT NULL,
-  `longitude` DECIMAL(11, 8) NOT NULL,
-  `date` DATE,
-  `base64img` TEXT NOT NULL,  
+  `name` varchar(255) DEFAULT NULL,
+  `review` varchar(255) DEFAULT NULL,
+  `latitude` decimal(10,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  `date` date DEFAULT NULL,
+  `base64img` text NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
